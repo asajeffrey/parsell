7e87764a191d2f8075746854f983ca3b3eb8c11f
Alan Jeffrey <ajeffrey@mozilla.com>
2016-01-08 09:18:16 -0600

    Bikeshedding.

--- src/parser/combinators.rs
+++ src/parser/combinators.rs
@@ -9,19 +9,19 @@ use self::MatchResult::{WillMatch, MightMatch, WontMatch, MatchedAll, MatchedSom
 // Borrowing encoding of paramaterized types from
 // https://github.com/rust-lang/rfcs/blob/master/text/0195-associated-items.md#encoding-higher-kinded-types
 
-pub trait RefType<'a> {
-    type Ref;
+pub trait TypeWithLifetime<'a> {
+    type Type;
 }
 
-pub type Ref<'a,T> where T: RefType<'a> = T::Ref;
+pub type At<'a,T> where T: TypeWithLifetime<'a> = T::Type;
 
-pub struct ConstRef<T> (T);
+pub struct Always<T> (T);
 
-pub type UnitRef = ConstRef<()>;
-
-impl<'a,T> RefType<'a> for ConstRef<T> {
-    type Ref = T;
+impl<'a,T> TypeWithLifetime<'a> for Always<T> {
+    type Type = T;
 }
+
+pub type Unit = Always<()>;
 
 #[derive(Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Debug)]
 pub enum MatchResult<T> {
@@ -32,28 +32,28 @@ pub enum MatchResult<T> {
     MatchedSome(T,T),
 }
 
-pub trait Matcher<T> where T: 'static+for<'a> RefType<'a> {
+pub trait Matcher<T> where T: 'static+for<'a> TypeWithLifetime<'a> {
     // If push returns MightMatch or WontMatch, it is side-effect-free
-    fn push<'a>(&mut self, value: Ref<'a,T>) -> MatchResult<Ref<'a,T>>;
+    fn push<'a>(&mut self, value: At<'a,T>) -> MatchResult<At<'a,T>>;
     // Resets the matcher state back to its initial state
     fn done(&mut self);
 }
 
 // ----------- Types for parsers ------------
 
-pub trait Consumer<T> where T: 'static+for<'a> RefType<'a> {
-    fn accept<'a>(&mut self, arg: Ref<'a,T>);
+pub trait Consumer<T> where T: 'static+for<'a> TypeWithLifetime<'a> {
+    fn accept<'a>(&mut self, arg: At<'a,T>);
 }
 
-pub trait Parser<S,T> where S: 'static+for<'a> RefType<'a>, T: 'static+for<'a> RefType<'a> {
-    fn push<'a>(&mut self, value: Ref<'a,S>, downstream: &mut Consumer<T>) -> MatchResult<Ref<'a,S>>;
+pub trait Parser<S,T> where S: 'static+for<'a> TypeWithLifetime<'a>, T: 'static+for<'a> TypeWithLifetime<'a> {
+    fn push<'a>(&mut self, value: At<'a,S>, downstream: &mut Consumer<T>) -> MatchResult<At<'a,S>>;
     fn done(&mut self, downstream: &mut Consumer<T>);
     // fn and_then<R: Parser<S,T>>(self, other: R) -> AndThenParser<Self,R> {
     //     AndThenParser{ lhs: self, rhs: other, in_lhs: true }
     // }
 }
 
-pub trait BufferableMatcher<S,T> where S: 'static+for<'a> RefType<'a>, T: Parser<S,S> {
+pub trait BufferableMatcher<S,T> where S: 'static+for<'a> TypeWithLifetime<'a>, T: Parser<S,S> {
     fn buffer(self) -> T;
 }
 
@@ -65,8 +65,8 @@ pub struct AndThenParser<L,R> {
     in_lhs: bool,
 }
 
-impl<S,T,L,R> Parser<S,T> for AndThenParser<L,R> where L: Parser<S,T>, R: Parser<S,T>, S: 'static+for<'a> RefType<'a>, T: 'static+for<'a> RefType<'a>  {
-    fn push<'a>(&mut self, value: Ref<'a,S>, downstream: &mut Consumer<T>) -> MatchResult<Ref<'a,S>> {
+impl<S,T,L,R> Parser<S,T> for AndThenParser<L,R> where L: Parser<S,T>, R: Parser<S,T>, S: 'static+for<'a> TypeWithLifetime<'a>, T: 'static+for<'a> TypeWithLifetime<'a>  {
+    fn push<'a>(&mut self, value: At<'a,S>, downstream: &mut Consumer<T>) -> MatchResult<At<'a,S>> {
         if self.in_lhs {
             match self.lhs.push(value, downstream) {
                 WillMatch => MightMatch, // TODO: we are returning MightMatch even though lhs may have side-effects
@@ -88,10 +88,10 @@ impl<S,T,L,R> Parser<S,T> for AndThenParser<L,R> where L: Parser<S,T>, R: Parser
 
 // ----------- Matching strings -------------
 
-pub struct StrRef;
+pub struct Str;
 
-impl<'a> RefType<'a> for StrRef {
-    type Ref = &'a str;
+impl<'a> TypeWithLifetime<'a> for Str {
+    type Type = &'a str;
 }
 
 // ----------- Constant parsers -------------
@@ -101,8 +101,8 @@ pub struct ConstantParser {
     offset: Option<usize>,
 }
 
-impl Parser<StrRef,UnitRef> for ConstantParser {
-    fn push<'a>(&mut self, string: &'a str, downstream: &mut Consumer<UnitRef>) -> MatchResult<&'a str> {
+impl Parser<Str,Unit> for ConstantParser {
+    fn push<'a>(&mut self, string: &'a str, downstream: &mut Consumer<Unit>) -> MatchResult<&'a str> {
         match self.offset.take() {
             None => MatchedSome("",string),
             Some(index) if string == &self.constant[index..]           => { downstream.accept(()); MatchedAll },
@@ -111,7 +111,7 @@ impl Parser<StrRef,UnitRef> for ConstantParser {
             Some(_)                                                    => { WontMatch },
         }
     }
-    fn done(&mut self, _: &mut Consumer<UnitRef>) {
+    fn done(&mut self, _: &mut Consumer<Unit>) {
         self.offset = Some(0);
     }
 }
@@ -120,7 +120,7 @@ pub fn constant(string: String) -> ConstantParser {
     ConstantParser{ constant: string, offset: Some(0) }
 }
 
-// If m is a Matcher<StrRef> then m.buffer() is a Parser<StrRef,StrRef>.
+// If m is a Matcher<Str> then m.buffer() is a Parser<Str,Str>.
 // It does as little buffering as it can, but it does allocate as buffer for the case
 // where the boundary marker of the input is misaligned with that of the matcher.
 // For example, m is matching string literals, and the input is '"abc' followed by 'def"'
@@ -142,8 +142,8 @@ pub struct BufferedParser<S> {
     state: BufferedParserState,
 }
 
-impl<S> Parser<StrRef,StrRef> for BufferedParser<S> where S: Matcher<StrRef> {
-    fn push<'a>(&mut self, string: &'a str, downstream: &mut Consumer<StrRef>) -> MatchResult<&'a str> {
+impl<S> Parser<Str,Str> for BufferedParser<S> where S: Matcher<Str> {
+    fn push<'a>(&mut self, string: &'a str, downstream: &mut Consumer<Str>) -> MatchResult<&'a str> {
         match mem::replace(&mut self.state, End) {
             Beginning => {
                 let result = self.matcher.push(string);
@@ -170,7 +170,7 @@ impl<S> Parser<StrRef,StrRef> for BufferedParser<S> where S: Matcher<StrRef> {
             End => MatchedSome("",string),
         }
     }
-    fn done(&mut self, downstream: &mut Consumer<StrRef>) {
+    fn done(&mut self, downstream: &mut Consumer<Str>) {
         match mem::replace(&mut self.state, Beginning) {
             Middle(buffer, Will) => downstream.accept(&*buffer),
             _                    => (),
@@ -178,7 +178,7 @@ impl<S> Parser<StrRef,StrRef> for BufferedParser<S> where S: Matcher<StrRef> {
     }
 }
 
-impl<S> BufferableMatcher<StrRef,BufferedParser<S>> for S where S: Matcher<StrRef> {
+impl<S> BufferableMatcher<Str,BufferedParser<S>> for S where S: Matcher<Str> {
     fn buffer(self) -> BufferedParser<S> {
         BufferedParser{ matcher: self, state: Beginning }
     }
