18fa0a111772fc41fc49c648929ed13c6b8e6290
Alan Jeffrey <ajeffrey@mozilla.com>
2016-01-12 10:49:52 -0600

    Issue a Whitespace token for each whitespace char.

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -33,7 +33,7 @@ impl Function<Str,Token> for MkIdentifier {
 pub fn lexer<C>(consumer: &mut C) where C: ParserConsumer<Str,Token> {
     let LPAREN = string("(").map(LParen);
     let RPAREN = string(")").map(RParen);
-    let WHITESPACE = character(char::is_whitespace).plus().map(Whitespace);
+    let WHITESPACE = character(char::is_whitespace).map(Whitespace);
     let IDENTIFIER = character(char::is_alphabetic).and_then(character(char::is_alphanumeric).star()).buffer().map(MkIdentifier);
     let TOKEN = LPAREN.or_else(RPAREN).or_else(WHITESPACE).or_else(IDENTIFIER);
     consumer.accept(TOKEN.star())
