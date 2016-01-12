cd7e387b39b62f160684ff9cdcf228c1e0c841eb
Alan Jeffrey <ajeffrey@mozilla.com>
2016-01-12 10:13:03 -0600

    First draft lexer().

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,5 +1,7 @@
-use parser::combinators::{TypeWithLifetime, At, Str, Unit, Parser, StringParser, string, character};
+use parser::combinators::{TypeWithLifetime, Str, Unit, Function, Parser, StrParser, ParserConsumer, string, character};
+use self::TokenAt::{LParen, RParen, Whitespace, Identifier};
 
+#[derive(Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Debug)]
 pub enum TokenAt<'a> {
     LParen,
     RParen,
@@ -9,14 +11,31 @@ pub enum TokenAt<'a> {
 
 pub struct Token;
 
-impl<'a> TypeWithLifetime<'a> for TokenAt<'a> {
+impl<'a> TypeWithLifetime<'a> for Token {
     type Type = TokenAt<'a>;
 }
 
-fn foo() {
-    let LPAREN = string("(");
-    let RPAREN = string(")");
-    let WHITESPACE = character(char::is_whitespace).plus();
-    let IDENTIFIER = character(char::is_alphabetic).and_then(character(char::is_alphanumeric).star());
+impl Function<Unit,Token> for TokenAt<'static> {
+    fn apply<'a>(&self, _: ()) -> TokenAt<'a> {
+        self.clone()
+    }
+}
+
+struct MkIdentifier;
+
+impl Function<Str,Token> for MkIdentifier {
+    fn apply<'a>(&self, name: &'a str) -> TokenAt<'a> {
+        Identifier(name)
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn lexer<C>(consumer: &mut C) where C: ParserConsumer<Str,Token> {
+    let LPAREN = string("(").map(LParen);
+    let RPAREN = string(")").map(RParen);
+    let WHITESPACE = character(char::is_whitespace).plus().map(Whitespace);
+    let IDENTIFIER = character(char::is_alphabetic).and_then(character(char::is_alphanumeric).star()).buffer().map(MkIdentifier);
+    let TOKEN = LPAREN.or_else(RPAREN).or_else(WHITESPACE).or_else(IDENTIFIER);
+    consumer.accept(TOKEN.star())
 }
 
