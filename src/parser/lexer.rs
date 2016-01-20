f1ad1d74e3530c58d39bfdd461311976fe9aff48
Alan Jeffrey <ajeffrey@mozilla.com>
2016-01-20 17:19:43 -0600

    Rewrote lexer and parser.

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,13 +1,28 @@
-use parser::combinators::{Parser, StrParser, ParseTo, Consumer, string, character};
+use parser::combinators::{Parser, ParseTo, Consumer, ErrConsumer, character, character_match};
 use self::Token::{Begin, End, Identifier, Number, Whitespace};
+use self::LexError::{ShouldntHappen};
+use std::num::ParseIntError;
 
-#[derive(Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Debug)]
+#[derive(Copy, Clone, Eq, Hash, Ord, PartialOrd, Debug)]
 pub enum Token<'a> {
     Begin(&'a str),
     End,
     Identifier(&'a str),
     Number(usize),
     Whitespace(&'a str),
+}
+
+impl<'a,'b> PartialEq<Token<'a>> for Token<'b> {
+    fn eq(&self, other: &Token<'a>) -> bool {
+        match (*self, *other) {
+            (Begin(s),      Begin(t))      => s == t,
+            (End,           End)           => true,
+            (Identifier(x), Identifier(y)) => x == y,
+            (Number(m),     Number(n))     => m == n,
+            (Whitespace(s), Whitespace(t)) => s == t,
+            _                              => false,
+        }
+    }
 }
 
 impl<'a> From<Token<'a>> for String {
@@ -22,45 +37,59 @@ impl<'a> From<Token<'a>> for String {
     }
 }
 
-pub trait LexerConsumer<D> where D: for<'a> Consumer<Token<'a>> {
+#[derive(Clone, Debug)]
+pub enum LexError {
+    ShouldntHappen(ParseIntError),
+}
+
+impl From<ParseIntError> for LexError {
+    fn from(err: ParseIntError) -> LexError {
+        ShouldntHappen(err)
+    }
+}
+
+pub trait LexerConsumer<D> where D: ErrConsumer<LexError>+for<'a> Consumer<Token<'a>> {
     fn accept<L>(self, lexer: L) where L: for<'a> ParseTo<&'a str,D>;
 }
 
-fn is_lparen(ch: char) -> bool { ch == '(' }
-fn is_rparen(ch: char) -> bool { ch == ')' }
-fn is_dollar(ch: char) -> bool { ch == '$' }
 fn is_keyword_char(ch: char) -> bool { ch.is_alphanumeric() || (ch == '.') }
 fn is_identifier_char(ch: char) -> bool { ch.is_alphanumeric() || (ch == '.') || (ch == '$') }
 
 fn mk_begin<'a>(s: &'a str) -> Token<'a> { Begin(s) }
-fn mk_end<'a>(ch: char) -> Token<'a> { End }
+fn mk_end<'a>(_: char) -> Token<'a> { End }
 fn mk_identifier<'a>(s: &'a str) -> Token<'a> { Identifier(s) }
-fn mk_number<'a>(s: &'a str) -> Token<'a> { Number(usize::from_str_radix(s,10).unwrap()) }
+fn mk_number<'a>(s: &'a str) -> Result<Token<'a>,LexError> { Ok(Number(try!(usize::from_str_radix(s,10)))) }
 fn mk_whitespace<'a>(s: &'a str) -> Token<'a> { Whitespace(s) }
 
 #[allow(non_snake_case)]
-pub fn lexer<C,D>(consumer: C) where C: LexerConsumer<D>, D: for<'a> Consumer<Token<'a>> {
-    let BEGIN = character(is_lparen).ignore().and_then(character(is_keyword_char).plus().buffer().map(mk_begin));
-    let END = character(is_rparen).map(mk_end);
-    let WHITESPACE = character(char::is_whitespace).plus().buffer().map(mk_whitespace);
-    let IDENTIFIER = character(is_identifier_char).plus().buffer().map(mk_identifier);
-    let NUMBER = character(char::is_numeric).plus().buffer().map(mk_number);
+pub fn lexer<C,D>(consumer: C) where C: LexerConsumer<D>, D: ErrConsumer<LexError>+for<'a> Consumer<Token<'a>> {
+    let BEGIN = character('(').ignore().and_then(character_match(is_keyword_char).plus().buffer().map(mk_begin));
+    let END = character(')').map(mk_end);
+    let WHITESPACE = character_match(char::is_whitespace).plus().buffer().map(mk_whitespace);
+    let IDENTIFIER = character_match(is_identifier_char).plus().buffer().map(mk_identifier);
+    let NUMBER = character_match(char::is_numeric).plus().buffer().map(mk_number).results();
     consumer.accept(BEGIN.or_else(END).or_else(IDENTIFIER).or_else(NUMBER).or_else(WHITESPACE).star())
 }
 
 #[test]
 fn test_lexer() {
-    struct TestConsumer(Vec<String>);
+    struct TestConsumer(Vec<String>,Vec<LexError>);
     impl<'a> Consumer<Token<'a>> for TestConsumer {
         fn accept(&mut self, tok: Token<'a>) {
             self.0.push(String::from(tok));
         }
     }
+    impl ErrConsumer<LexError> for TestConsumer {
+        fn error(&mut self, err: LexError) {
+            self.1.push(err);
+        }
+    }
     impl LexerConsumer<TestConsumer> for TestConsumer {
         fn accept<L>(mut self, mut lex: L) where L: for<'a> ParseTo<&'a str,TestConsumer> {
             lex.push_to("(a123 $bcd f32 \t 37)", &mut self);
-            assert_eq!(self.0, vec!["(a123", " ", "$bcd", " ", "f32", " \t ", "37", ")"]);
+            assert_eq!(self.0, ["(a123", " ", "$bcd", " ", "f32", " \t ", "37", ")"]);
+            assert_eq!(self.1.len(), 0);
         }
     }
-    lexer(TestConsumer(Vec::new()));
+    lexer(TestConsumer(Vec::new(),Vec::new()));
 }
--- src/parser/text.rs
+++ src/parser/text.rs
@@ -1,4 +1,4 @@
-use parser::combinators::{Parser, ParseTo, Consumer, token, tokens};
+use parser::combinators::{Parser, ParseTo, Consumer, ErrConsumer, token, token_match};
 use parser::lexer::{Token};
 use parser::lexer::Token::{Begin, End, Identifier, Number};
 
@@ -9,9 +9,10 @@ use ast::Typ::{F32, F64, I32, I64};
 pub enum ParseError {
     ExpectingTyp(String),
     ExpectingId(String),
+    ExpectingNumber(String),
 }
 
-pub trait ParserConsumer<T,D> where D: Consumer<Result<T,ParseError>> {
+pub trait ParserConsumer<D> where D: Consumer<Module>+ErrConsumer<ParseError> {
     fn accept<P>(self, parser: P) where P: for<'a> ParseTo<&'a[Token<'a>],D>;
 }
 
@@ -22,10 +23,24 @@ fn is_identifier<'a>(tok: Token<'a>) -> bool {
     }
 }
 
+fn is_number<'a>(tok: Token<'a>) -> bool {
+    match tok {
+        Number(_) => true,
+        _         => false,
+    }
+}
+
 fn is_begin_memory<'a>(tok: Token<'a>) -> bool { tok == Begin("memory") }
 fn is_begin_module<'a>(tok: Token<'a>) -> bool { tok == Begin("module") }
 fn is_begin_param<'a>(tok: Token<'a>) -> bool { tok == Begin("param") }
 fn is_end<'a>(tok: Token<'a>) -> bool { tok == End }
+
+fn mk_usize<'a>(tok: Token<'a>) -> Result<usize,ParseError> {
+    match tok {
+        Number(n) => Ok(n),
+        _         => Err(ParseError::ExpectingNumber(String::from(tok))),
+    }
+}
 
 fn mk_typ<'a>(tok: Token<'a>) -> Result<Typ,ParseError> {
     match tok {
@@ -44,23 +59,33 @@ fn mk_id<'a>(tok: Token<'a>) -> Result<String,ParseError> {
     }
 }
 
-fn mk_var(children: (Result<String,ParseError>, Result<Typ,ParseError>))-> Result<Var,ParseError> {
-    Ok(Var{ name: try!(children.0), typ: try!(children.1) })
+fn mk_var(children: (String,Typ)) -> Var {
+    Var{ name: children.0, typ: children.1 }
 }
 
-fn mk_memory<'a>(children: Result<usize,ParseError>) -> Result<Memory,ParseError> {
-    Ok(Memory{ init: try!(children), max: None, segments: vec![] })
+fn mk_memory<'a>(children: usize) -> Memory {
+    Memory{ init: children, max: None, segments: vec![] }
 }
 
-fn mk_module<'a>(children: Token<'a>) -> Result<Module,ParseError> {
-    Ok(Module{ memory: None, imports: vec![], exports: vec![], functions: vec![] })
+fn mk_module<'a>(children: Memory) -> Module {
+    Module{ memory: Some(children), imports: vec![], exports: vec![], functions: vec![] }
 }
 
-pub fn parser<C,D>(consumer: C) where C: ParserConsumer<Module,D>, D: Consumer<Result<Module,ParseError>> {
-    let typ = token(is_identifier).map(mk_typ);
-    let id = token(is_identifier).map(mk_id);
-    let param = token(is_begin_param).ignore().and_then(id).zip(typ).and_then(token(is_end).ignore()).map(mk_var);
-    let module = token(is_begin_module).and_then(token(is_end).ignore()).map(mk_module);
+pub fn parser<C,D>(consumer: C) where C: ParserConsumer<D>, D: Consumer<Module>+ErrConsumer<ParseError> {
+    let typ = token_match(is_identifier)
+        .map(mk_typ).results();
+    let id = token_match(is_identifier)
+        .map(mk_id).results();
+    let number = token_match(is_number)
+        .map(mk_usize).results();
+    let memory = token_match(is_begin_memory).ignore()
+        .and_then(number)
+        .and_then(token_match(is_end).ignore())
+        .map(mk_memory);
+    let module = token_match(is_begin_module).ignore()
+        .and_then(memory)
+        .and_then(token_match(is_end).ignore())
+        .map(mk_module);
     let top_level = module;
     consumer.accept(top_level);
 }
@@ -68,28 +93,44 @@ pub fn parser<C,D>(consumer: C) where C: ParserConsumer<Module,D>, D: Consumer<R
 #[test]
 fn test_parser() {
     use parser::combinators::MatchResult::{Matched,Failed};
-    struct TestConsumer;
-    impl ParserConsumer<Module,Vec<Result<Module,ParseError>>> for TestConsumer {
-        fn accept<P>(self, mut parser: P) where P: for<'a> ParseTo<&'a[Token<'a>],Vec<Result<Module,ParseError>>> {
-            let mut results = Vec::new();
+    struct TestConsumer(Vec<Module>,Vec<ParseError>);
+    impl Consumer<Module> for TestConsumer {
+        fn accept(&mut self, module: Module) {
+            self.0.push(module);
+        }
+    }
+    impl ErrConsumer<ParseError> for TestConsumer {
+        fn error(&mut self, err: ParseError) {
+            self.1.push(err);
+        }
+    }
+    impl ParserConsumer<TestConsumer> for TestConsumer {
+        fn accept<P>(mut self, mut parser: P) where P: for<'a> ParseTo<&'a[Token<'a>],TestConsumer> {
             let tokens = [
-                Begin("module"),                
+                Begin("module"),
+                    Begin("memory"),
+                        Number(1024),
+                    End,
                 End,
             ];
             let ast = Module{
-                memory: None,
+                memory: Some(Memory{
+                    init: 1024,
+                    max: None,
+                    segments: vec![],
+                }),
                 imports: vec![],
                 exports: vec![],
                 functions: vec![]
             };
-            assert_eq!(parser.done_to(&mut results), false);
-            assert_eq!(results, []);
-            results.clear();
-            assert_eq!(parser.push_to(&tokens, &mut results), Matched(None));
-            assert_eq!(parser.done_to(&mut results), true);
-            assert_eq!(results, [Ok(ast)]);
-            results.clear();
+            assert_eq!(parser.done_to(&mut self), false);
+            assert_eq!(self.0, []);
+            assert_eq!(self.1, []);
+            assert_eq!(parser.push_to(&tokens, &mut self), Matched(None));
+            assert_eq!(parser.done_to(&mut self), true);
+            assert_eq!(self.0, [ast]);
+            assert_eq!(self.1, []);
         }
     }
-    parser(TestConsumer);
+    parser(TestConsumer(Vec::new(),Vec::new()));
 }
