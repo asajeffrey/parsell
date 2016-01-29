215a2c8388dbb593794faf6df67d94389156507d
Alan Jeffrey <ajeffrey@mozilla.com>
2016-01-29 17:45:10 -0600

    Initial commit.

---
+++ target/doc/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['parsimonious'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='parsimonious/enum.Str.html' title='parsimonious::Str'>Str</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
