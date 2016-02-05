5e609944687c07020fc336291472c2d39c5b015d
Alan Jeffrey <ajeffrey@mozilla.com>
2016-02-05 13:03:09 -0600

    Regenerated docs.

--- target/doc/implementors/core/iter/trait.Iterator.js
+++ target/doc/implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['parsimonious'] = ["impl&lt;P, S&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='parsimonious/impls/struct.IterParser.html' title='parsimonious::impls::IterParser'>IterParser</a>&lt;P, P::State, S&gt; <span class='where'>where P: <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> + <a class='trait' href='parsimonious/trait.Committed.html' title='parsimonious::Committed'>Committed</a>&lt;S&gt;</span>",];
+implementors['parsell'] = ["impl&lt;P, S&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='parsell/impls/struct.IterParser.html' title='parsell::impls::IterParser'>IterParser</a>&lt;P, P::State, S&gt; <span class='where'>where P: <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> + <a class='trait' href='parsell/trait.Committed.html' title='parsell::Committed'>Committed</a>&lt;S&gt;</span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
