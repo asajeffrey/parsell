957407bdd611feff8a69611c76293fe8512716a4
Alan Jeffrey <ajeffrey@mozilla.com>
2016-02-25 14:15:09 -0600

    Added tests for typechecking time.

---
+++ tests/typecheck-time2.rs
@@ -0,0 +1,17 @@
+// Test for typechecking blow-up
+// https://github.com/rust-lang/rust/issues/31849
+
+extern crate parsell;
+use parsell::{Parser, UncommittedStr, CHARACTER};
+
+#[test]
+fn test_typecheck_time() {
+    CHARACTER
+        .and_then(CHARACTER
+        .and_then(CHARACTER
+        .and_then(CHARACTER
+        .and_then(CHARACTER
+        .and_then(CHARACTER
+        .and_then(CHARACTER))))))
+        .init_str("hello, world");
+}
